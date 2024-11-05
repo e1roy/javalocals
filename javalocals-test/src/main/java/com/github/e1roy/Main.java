420d0c1fbaf22d86f2d67d854f961aa78456d27a
elroy93 <elroyfunlife@gmail.com>
2024-11-06 01:52:12 +0800

    init

---
+++ javalocals-test/src/main/java/com/github/e1roy/Main.java
@@ -0,0 +1,12 @@
+package com.github.e1roy;
+
+/**
+ * @author elroysu
+ * @date 2024/11/6 星期三 1:41
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        new JvmLocalsTestFile().add(1, 2);
+    }
+}
---
+++ javalocals-test/src/test/java/com/github/e1roy/JvmLocalsTest.java
@@ -0,0 +1,16 @@
+package com.github.e1roy;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author elroysu
+ * @date 2024/11/6 星期三 1:41
+ */
+public class JvmLocalsTest {
+
+    @Test
+    public void JvmLocalsTestFile1() {
+        new JvmLocalsTestFile().add(1, 2);
+        new JvmLocalsTestFile().testLambda();
+    }
+}
